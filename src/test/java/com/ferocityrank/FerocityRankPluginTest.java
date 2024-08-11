6d401cc5a7a2007b9ff79325a0bec7c9d50c7814
kayqb <ferocity2max@gmail.com>
2024-08-11 20:36:17 +0100

    initial refactor

--- src/test/java/com/ferocityrank/FerocityRankPluginTest.java
+++ src/test/java/com/ferocityrank/FerocityRankPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.ferocityrank;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class FerocityRankPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(FerocityRankPlugin.class);
 		RuneLite.main(args);
 	}
 }
